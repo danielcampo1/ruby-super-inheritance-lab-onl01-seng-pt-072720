d6284b7db643b92aaf7733a488c91196216542df
daniel campo <campo_daniel94@yahoo.com>
2020-08-28 21:53:51 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -9,5 +9,5 @@ class ChattyStudent < Student
   
   def raise_hand
     super
-    puts "Pick me!" * 9
+    puts "Pick me!,Pick me!,Pick me!,Pick me!,Pick me!,Pick me!,Pick me!,Pick me!,Pick me!"
 end
